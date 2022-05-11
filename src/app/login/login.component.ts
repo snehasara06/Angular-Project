d634e14e77c3315213545c304ace43517d34d75e
Sneha Saravanakumar <sneha.sara.06@gmail.com>
2022-05-12 00:12:56 +0530

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+loginForm!:FormGroup;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loginForm=new FormGroup({
+      email:new FormControl(null,[
+        Validators.required,
+        Validators.email,
+        Validators.minLength(6)
+      ]),
+      password:new FormControl(null,Validators.required)
+    })
+  }
+  loginUser(){
+    console.log(this.loginForm.value);
+  }
+
+}
